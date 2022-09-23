9999b9f3e31bbebaf5f9ba35ab08d17e196c5e44
Marvellous Michaels Ubani <marvellousubani@gmail.com>
2022-09-23 14:20:30 +0100

    fixing gradle error1

--- src/app.ts
+++ src/app.ts
@@ -1,129 +1,105 @@
+
+let modifiedPageData = {};
 let currentPage = 1;
-const data: Record<number, Record<string, unknown>[]> = {};
+const dataPageView = document.querySelector('[data-pageview]') as HTMLElement | null;
+const dataPrevBtn = document.querySelector('[data-prevbtn]') as HTMLButtonElement | null;
+const dataNextBtn =  document.querySelector('[data-nextbtn]') as HTMLButtonElement | null;
 
-/**
- * We fetch the data, then render the current page
- */
-function startApp() {
-  fetchData(currentPage).then(() => {
-    renderCurrent();
-  });
+const startApp = async () => {
 
-  document
-    .querySelector('[data-prevbtn]')
-    ?.addEventListener('click', previousData);
+   // Onload
+   fetchUserData(currentPage).then(() => {
+    addData();
+  });    
+};
 
-  document.querySelector('[data-nextbtn]')?.addEventListener('click', nextData);
-}
-
-/**
- * It fetches data from an API and saves it in a global variable
- * @param {number} page - The page number to fetch.
- */
-async function fetchData(page: number) {
-  const response = await fetch(
-    'https://randomapi.com/api/8csrgnjw?key=LEIX-GF3O-AG7I-6J84&page=' + page
-  );
-
-  const { results } = await response.json();
-
-  const [res] = results;
-  Object.keys(res).forEach((key) => {
-    data[key] = res[key];
-  });
-}
-
-/**
- * Loop through data at currentPage and update dom
- */
-function renderCurrent() {
-  const tableBody: Element | null = document.querySelector('[data-sink]');
-  const prevBtn = document.querySelector('[data-prevbtn]');
-  const tableLabel = document.querySelector('[data-pageview]');
-
-  if (tableBody !== null) {
-    tableBody.innerHTML = '';
-  }
-
-  const tableData = data[currentPage];
-
-  /* Looping through the tableData and adding the data to the tableBody. */
-  for (let i = 0; i < tableData.length; i++) {
-    const data = tableData[i];
-
-    var row = `
-          <tr data-entryid=${data.id}>
-              <td>${data.row}</td>
-              <td>${data.gender}</td>
-              <td>${data.age}</td>
-          </tr> `;
-
-    if (tableBody !== null) {
-      tableBody.innerHTML += row;
+async function fetchUserData(page = 1) {
+    const response = await fetch(`https://randomapi.com/api/8csrgnjw?key=LEIX-GF3O-AG7I-6J84&page=${page}`);
+    if (!response.ok) {
+      const message = `An error has occured: ${response.status}`;
+      throw new Error(message);
     }
+    const userData = await response.json();
+
+
+    // Caching & Modifying the fetched data into object
+    modifiedPageData[currentPage] = userData['results'][0][currentPage];
+    modifiedPageData[currentPage + 1] = userData['results'][0][currentPage + 1];
+
+};
+
+
+// Function to Build DOM
+
+ function addData(){
+    let tBody = document.querySelector("tbody");
+    tBody?.replaceChildren();
+
+    let results = modifiedPageData[currentPage];
+    let previousDisabled = currentPage == 1 ? true : false;
+    
+    let currentBuild = ``;
+
+    if(tBody != undefined){
+        tBody.innerHTML = "";
+      }
+
+    
+
+    for(let i = 0; i < results.length; i++){
+        const {id, age, gender, row} = results[i];
+        currentBuild += `<tr data-entryid ='${id}'><td>${row}</td><td>${gender}</td><td>${age}</td> </tr>`
+    }
+
+   // Showing current page on UI
+   if(dataPageView != undefined){
+        dataPageView.innerHTML = `Showing Page ${currentPage}`;
+        dataPageView.dataset.pageview = String(currentPage);
+    }
+
+   // Determine previous button state
+    if(dataPrevBtn != undefined){
+        dataPrevBtn.disabled = previousDisabled;
+        dataPrevBtn.dataset.prevbtn = String(currentPage-1);
+    }
+
+  // Next Button Data state
+  if(dataNextBtn != undefined){
+    dataNextBtn.dataset.nextbtn = String(currentPage+1);
   }
-
-  if (tableLabel !== null) {
-    tableLabel.innerHTML = 'Showing Page ' + currentPage;
+  
+  if(tBody != undefined){
+    tBody.innerHTML = currentBuild;
   }
+  document.querySelector('.page-container')?.classList.add("active");
+    
+ }
 
-  if (currentPage <= 1) {
-    // disable previous button
-    prevBtn?.setAttribute('disabled', 'true');
-  } else {
-    prevBtn?.removeAttribute('disabled');
-  }
-}
+ function nextData(){
+    // If its an odd page then there's no data to fetch
+    currentPage++;
+    console.log(modifiedPageData[currentPage]);
+    if(modifiedPageData[currentPage]){
+        addData();
+        return;
+    }
 
-/**
- * "Given a number, return true if it's odd, false otherwise."
- *
- * The function takes a number as an argument and returns true if it's odd, false otherwise
- * @param {number} number - number
- * @returns The function isOdd is being returned.
- */
-function isOdd(number: number) {
-  return number % 2 !== 0;
-}
+    fetchUserData(currentPage).then(() => {
+        addData();
+      });
 
-/**
- * If the current page is odd, we can just increment the page and render the current page. If the
- * current page is even, we need to fetch the next page of data before we can render the current page
- * @returns the value of the currentPage variable.
- */
-function nextData() {
-  if (isOdd(currentPage)) {
-    currentPage += 1;
-    renderCurrent();
-    return;
-  }
+ }
 
-  currentPage += 1;
-  fetchData(currentPage).then(() => {
-    renderCurrent();
-  });
-}
 
-/**
- * If the current page is odd, fetch the previous page and render the current page. If the current page
- * is even, just render the previous page
- * @returns the currentPage.
- */
-function previousData() {
-  if (currentPage <= 1) {
-    return;
-  }
-  if (isOdd(currentPage)) {
-    currentPage -= 2;
-    fetchData(currentPage).then(() => {
-      currentPage += 1;
-      renderCurrent();
-    });
-    return;
-  }
+ dataNextBtn?.addEventListener('click', () => {
+    nextData();
+ });
 
-  currentPage -= 1;
-  renderCurrent();
-}
+ dataPrevBtn?.addEventListener('click', () => {
+    currentPage--;
+    addData();
+ });
+
+
 
 document.addEventListener('DOMContentLoaded', startApp);
